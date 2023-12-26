886f96af922383446f172efc932e3dc91af68120
yeatsg <yeatsg@gmail.com>
2023-12-26 11:28:59 -0500

    Quick Update
    Creating basic C# boilerplate.

---
+++ index.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Your application logic goes here
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
